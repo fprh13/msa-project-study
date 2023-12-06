e92712a5d129e676f8419648f624fccd7d7d7ee4
fprh13 <fprh13@gamil.com>
2023-12-07 02:48:41 +0900

    feat: spring gateway filter 기능 추가
    log 로 확인 가능
    login 관련 filter로 변경 예정

--- second-service/src/main/java/com/example/secondservice/SecondApi.java
+++ second-service/src/main/java/com/example/secondservice/SecondApi.java
@@ -1,16 +1,30 @@
 package com.example.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/second-service")
 public class SecondApi {
 
     @GetMapping("/welcome")
-    public String fistRequest() {
+    public String secondRequest() {
         return "Hello? welcome, second-service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return " Hello world in Second Service";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Hi, there. this is a message from Second Service.";
     }
 
 }
