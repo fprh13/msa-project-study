aee0eea54389e48f5e7c3fd7ff9362fbd55b2ef3
fprh13 <fprh13@gamil.com>
2023-12-08 02:03:54 +0900

    feat: spring cloud eureka 연동 완료
    loadBalancer를 이용하여 분산 처리

--- first-service/src/main/java/com/example/firstservice/FirstApi.java
+++ first-service/src/main/java/com/example/firstservice/FirstApi.java
@@ -1,6 +1,9 @@
 package com.example.firstservice;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/first-service")
+@RequiredArgsConstructor
 public class FirstApi {
+
+    // port 정보를 확인하는 법
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String fistRequest() {
@@ -24,8 +31,11 @@ public class FirstApi {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. this is a message from First Service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}",request.getServerPort());
+        return String.format(
+                "Hi, there. this is a message from First Service om PORT %s.",
+                env.getProperty("local.server.port"));
     }
 
 }
--- gateway-service/src/main/java/com/example/gatewayservice/filter/GlobalFilter.java
+++ gateway-service/src/main/java/com/example/gatewayservice/filter/GlobalFilter.java
@@ -1,5 +1,6 @@
 package com.example.gatewayservice.filter;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
@@ -8,13 +9,11 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
-import java.io.ObjectInputFilter;
-
 // 개별적으로 yml 정의를 해줘야한다.
 @Component
 @Slf4j
-public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
-    public CustomFilter() {
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+    public GlobalFilter() {
         super(Config.class);
     }
 
@@ -23,16 +22,25 @@ public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Conf
         return ((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
-            log.info("Custom PRE filter: request id -> {}", request.getId());
-            // Custom Post Filter
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                log.info("Custom POST filter: response code -> {}", response.getStatusCode());
-            }));
 
+            log.info("Global filter baseMessage: {}", config.getBaseMessage());
+
+            if (config.isPreLogger()) {
+                log.info("Global Filter start: request id -> {}", request.getId());
+            }
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if (config.isPostLogger()) {
+                    log.info("Global Filter End: response code -> {}", response.getStatusCode());
+                }
+            }));
         });
     }
 
+    @Data
     public static class Config {
-        // put the configuration properties
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
     }
 }
